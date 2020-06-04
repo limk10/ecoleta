408b851356d7591a03e2ce072827d2a51dfed08b
Matheus Lopes <matheus_barbosa5@hotmail.com>
2020-06-04 00:45:25 -0400

    Ajustas eslint e prettier nos projetos

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,33 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+  'Diego',
+  'Matheus',
+  'Eduardo',
+  'Eleno',
+  'Chaves'
+];
+
 app.get('/users', (request, response) => {
-  response.json([
-    'Diego',
-    'Matheus',
-    'Eduardo',
-    'Eleno',
-    'Chaves'
-  ]);
+  const search = String(request.query.search);
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+  response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const user = users[id];
+  response.json(user);
+});
+
+app.post('/users', (request, response) => {
+  const user = request.body;
+
+  return response.json(user);
 });
 
 app.listen(3333);
